46f506a048a100d409ac2f9671c977fc8ed1ceb6
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-07-10 00:03:02 +0300

    changes

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const aboutEl = document.getElementById("about");
 const contactEl = document.getElementById("contact");
 const workel = document.getElementById("work");
 const helloel = document.getElementById("hello");
+const svgEl = document.getElementById("svg");
+window.addEventListener("scroll", rotateSvg);
+
+function rotateSvg () {
+    const rotationAngle = window.scrollY * 0.2;
+    svgEl.style.transform = `rotate(${rotationAngle}deg)`;
+}
 
 aboutEl.addEventListener("click", scrolEl)
 function scrolEl() {
